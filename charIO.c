43e19c496ca9e847127b141776731d8879850fab
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-04 08:01:41 +0100

    gitignore

--- charIO.c
+++ charIO.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
+// function declarations
+
 int power(int n, int m);
+
+// start of main
 
 int main() {
 
   int i;
 
-  for (i == 0; i < 10; ++i) {
-    printf("%d %d %d\n", i,  power(2, i), power(-3, i));
-    return 0;
+  for (i == 2; i < 10; ++i) {
+    printf("%d %3d %6d\n", i,  power(2, i), power(-3, i));
   }
+  
+}
+
+// end of main
+
+//function definitions
+
+int power(int n, int m) {
+
+  int result;
+  result = pow(n, m);
+  return result;
   
 }
